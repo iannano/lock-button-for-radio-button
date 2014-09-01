3e6690afa491544c99275a8f0753f95c4f5c8d6b
Ibnu Aulia Nugraha <ibnuauliana@gmail.com>
2014-09-01 11:04:43 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Disable Radio Button In Multiple Form</title>
+<script type="text/javascript" src="Disable Radio Box In Multiple Form - JS.js"></script>
+</head>
+
+<body>
+<p>
+	<form name="form1">
+		<input type="radio" name="radio1" id="radio1" value="1"> Radio Satu<br>
+    	<input type="radio" name="radio1" id="radio2" value="2"> Radio Dua<br>
+    	<input type="button" id="button" onClick="validasi1()" value="Lock" h><br>
+	</form>
+</p>
+<p>
+	<form name="form2">
+		<input type="radio" name="radio1" id="radio3" value="1"> Radio Satu<br>
+    	<input type="radio" name="radio1" id="radio4" value="2"> Radio Dua<br>
+    	<input type="button" onClick="validasi2()" value="Lock"><br>
+	</form>
+</p>    
+</body>
+</html>
